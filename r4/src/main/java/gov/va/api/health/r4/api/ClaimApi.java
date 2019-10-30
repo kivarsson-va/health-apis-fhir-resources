0013582c2e365c1109bf660027c673e1c4016b70
alecbert-va <43882474+alecbert-va@users.noreply.github.com>
2019-10-30 07:25:01 -0500

    Claim API for reads only to support OpenAPI documentation (#28)

---
+++ r4/src/main/java/gov/va/api/health/r4/api/ClaimApi.java
@@ -0,0 +1,51 @@
+package gov.va.api.health.r4.api;
+
+import gov.va.api.health.r4.api.resources.Claim;
+import gov.va.api.health.r4.api.resources.OperationOutcome;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+public interface ClaimApi {
+
+  @Operation(
+    summary = "Claim Read",
+    description = "https://www.hl7.org/fhir/claim.html",
+    tags = {"Claim"}
+  )
+  @GET
+  @Path("Claim/{id}")
+  @ApiResponse(
+    responseCode = "200",
+    description = "Record found",
+    content =
+        @Content(
+          mediaType = "application/json+fhir",
+          schema = @Schema(implementation = Claim.class)
+        )
+  )
+  @ApiResponse(
+    responseCode = "400",
+    description = "Bad request",
+    content =
+        @Content(
+          mediaType = "application/json+fhir",
+          schema = @Schema(implementation = OperationOutcome.class)
+        )
+  )
+  @ApiResponse(
+    responseCode = "404",
+    description = "Not found",
+    content =
+        @Content(
+          mediaType = "application/json+fhir",
+          schema = @Schema(implementation = OperationOutcome.class)
+        )
+  )
+  Claim claimRead(@Parameter(in = ParameterIn.PATH, name = "id", required = true) String id);
+}
