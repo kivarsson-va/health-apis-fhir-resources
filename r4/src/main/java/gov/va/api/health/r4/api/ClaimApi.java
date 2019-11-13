34b621905d008c20e1b7eb7382113d10b03f414f
lbrown-va <52166122+lbrown-va@users.noreply.github.com>
2019-11-13 15:56:22 -0500

    Add claimSearch to ClaimApi. (#34)

--- r4/src/main/java/gov/va/api/health/r4/api/ClaimApi.java
+++ r4/src/main/java/gov/va/api/health/r4/api/ClaimApi.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
@@ -48,4 +49,43 @@ public interface ClaimApi {
         )
   )
   Claim claimRead(@Parameter(in = ParameterIn.PATH, name = "id", required = true) String id);
+
+  @Operation(
+    summary = "Claim Search",
+    description = "https://www.hl7.org/fhir/R4/claim.html",
+    tags = {"Claim"}
+  )
+  @GET
+  @Path("Claim")
+  @ApiResponse(
+    responseCode = "200",
+    description = "Record found",
+    content =
+        @Content(
+          mediaType = "application/json+fhir",
+          schema = @Schema(implementation = Claim.Bundle.class)
+        )
+  )
+  @ApiResponse(
+    responseCode = "404",
+    description = "Not found",
+    content =
+        @Content(
+          mediaType = "application/json+fhir",
+          schema = @Schema(implementation = OperationOutcome.class)
+        )
+  )
+  @ApiResponse(
+    responseCode = "400",
+    description = "Bad request",
+    content =
+        @Content(
+          mediaType = "application/json+fhir",
+          schema = @Schema(implementation = OperationOutcome.class)
+        )
+  )
+  Claim.Bundle claimSearch(
+      @Parameter(in = ParameterIn.QUERY, required = true, name = "patient") String id,
+      @Parameter(in = ParameterIn.QUERY, name = "page") @DefaultValue("1") int page,
+      @Parameter(in = ParameterIn.QUERY, name = "_count") @DefaultValue("15") int count);
 }
