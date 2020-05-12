055d8ccb63f7a099f31366986d370ed68b176514
bryan.schofield <vasdvp.jenkins@libertyits.com>
2020-05-12 18:46:48 +0000

    Release 2.0.40 - Jenkins Build 119 (department-of-veterans-affairs/health-apis-fhir-resources/master)

--- us-core-r4/src/main/java/gov/va/api/health/uscorer4/api/PatientApi.java
+++ us-core-r4/src/main/java/gov/va/api/health/uscorer4/api/PatientApi.java
@@ -1,14 +1,13 @@
 package gov.va.api.health.uscorer4.api;
 
-import gov.va.api.health.uscorer4.api.resources.Patient;
 import gov.va.api.health.r4.api.resources.OperationOutcome;
+import gov.va.api.health.uscorer4.api.resources.Patient;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -16,7 +15,8 @@ import javax.ws.rs.Path;
 public interface PatientApi {
   @Operation(
       summary = "Patient Read",
-      description = "https://build.fhir.org/ig/HL7/US-Core-R4/StructureDefinition-us-core-patient.html",
+      description =
+          "https://build.fhir.org/ig/HL7/US-Core-R4/StructureDefinition-us-core-patient.html",
       tags = {"Patient"})
   @GET
   @Path("Patient/{id}")
@@ -55,7 +55,8 @@ public interface PatientApi {
 
   @Operation(
       summary = "Patient Search",
-      description = "https://build.fhir.org/ig/HL7/US-Core-R4/StructureDefinition-us-core-patient.html",
+      description =
+          "https://build.fhir.org/ig/HL7/US-Core-R4/StructureDefinition-us-core-patient.html",
       tags = {"Patient"})
   @GET
   @Path("Patient")
