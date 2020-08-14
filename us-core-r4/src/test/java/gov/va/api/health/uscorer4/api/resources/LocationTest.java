d6cc56bca7b3b6cb903c042527a10b7875eee54d
Evan Clendenning <evan.clendenning@libertyits.com>
2020-08-14 15:20:49 -0400

    uscore r4 location model (#77)
    * uscore r4 location model

---
+++ us-core-r4/src/test/java/gov/va/api/health/uscorer4/api/resources/LocationTest.java
@@ -0,0 +1,52 @@
+package gov.va.api.health.uscorer4.api.resources;
+
+import static gov.va.api.health.uscorer4.api.RoundTrip.assertRoundTrip;
+
+import gov.va.api.health.r4.api.bundle.AbstractBundle;
+import gov.va.api.health.r4.api.bundle.BundleLink;
+import gov.va.api.health.uscorer4.api.samples.SampleLocations;
+import java.util.Collections;
+import org.junit.Test;
+
+public class LocationTest {
+  private final SampleLocations data = SampleLocations.get();
+
+  @Test
+  public void bundlerCanBuildLocationBundles() {
+    Location.Entry entry =
+        Location.Entry.builder()
+            .extension(Collections.singletonList(data.extension()))
+            .fullUrl("http://location.com")
+            .id("123")
+            .link(
+                Collections.singletonList(
+                    BundleLink.builder()
+                        .relation(BundleLink.LinkRelation.self)
+                        .url(("http://location.com/1"))
+                        .build()))
+            .resource(data.location())
+            .search(data.search())
+            .request(data.request())
+            .response(data.response())
+            .build();
+
+    Location.Bundle bundle =
+        Location.Bundle.builder()
+            .entry(Collections.singletonList(entry))
+            .link(
+                Collections.singletonList(
+                    BundleLink.builder()
+                        .relation(BundleLink.LinkRelation.self)
+                        .url(("http://location.com/2"))
+                        .build()))
+            .type(AbstractBundle.BundleType.searchset)
+            .build();
+
+    assertRoundTrip(bundle);
+  }
+
+  @Test
+  public void location() {
+    assertRoundTrip(data.location());
+  }
+}
