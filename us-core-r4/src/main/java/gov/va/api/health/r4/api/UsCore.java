9c8606cd6161928cb23a3d2fa708ed9d4f060961
bryan.schofield <vasdvp.jenkins@libertyits.com>
2020-09-24 15:41:29 +0000

    Release 5.0.2 - Jenkins Build 197 (department-of-veterans-affairs/health-apis-fhir-resources/master)

--- us-core-r4/src/main/java/gov/va/api/health/r4/api/UsCore.java
+++ us-core-r4/src/main/java/gov/va/api/health/r4/api/UsCore.java
@@ -6,8 +6,8 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * This annotation indicates fields that are required to be compliant with the UsCore
- * specification. (https://build.fhir.org/ig/HL7/US-Core-R4)
+ * This annotation indicates fields that are required to be compliant with the UsCore specification.
+ * (https://build.fhir.org/ig/HL7/US-Core-R4)
  */
 @Retention(RetentionPolicy.SOURCE)
 @Target({ElementType.FIELD, ElementType.CONSTRUCTOR, ElementType.METHOD, ElementType.PARAMETER})
